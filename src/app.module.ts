c3fbbc793f9f2b673246cb461a1c4a01b4214112
meirgoldshtein <m0548491057@gmail.com>
2024-10-15 11:28:42 +0300

    user dto done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { UserModule } from './user/user.module';
 import { NotesModule } from './notes/notes.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import 'dotenv/config'
 
 @Module({
   imports: [
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-    
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3, { message: 'Username must be at least 3 characters' })
+    username: string;
+
+    @IsEmail({}, { message: 'Email is not valid' })
+    @IsNotEmpty()
+    email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8, { message: 'Password must be at least 8 characters' })
+    password: string;
 }
